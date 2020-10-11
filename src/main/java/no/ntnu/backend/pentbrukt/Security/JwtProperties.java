081b553a2fa411dbbfd7a80d031d129c73fc2844
Kjetil <kjetilhammerseth@gmail.com>
2020-10-11 11:40:03 +0200

    Comment cleanup lol.

--- src/main/java/no/ntnu/backend/pentbrukt/Security/JwtProperties.java
+++ src/main/java/no/ntnu/backend/pentbrukt/Security/JwtProperties.java
@@ -7,14 +7,4 @@ public class JwtProperties {
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
 
-    /*
-
-    "Bearer: " works, but triggers exception
-
-    "Bearer " returns OK 200 but no json data...
-
-    WTF MOTHERFUCKER WHY AIDS HIV BITCH EBOLACUNT SPAGHETTIFUCKER
-
-     */
-
 }
